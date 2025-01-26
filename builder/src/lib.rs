ab9145affee4cc05641026a6fda7aa449e8a3aa7
Henry Chu <henrytech@outlook.com>
2025-01-26 21:51:13 +0800

    builder: test 02

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,25 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
 
-    TokenStream::new()
+    let vis = &input.vis;
+    let ident = &input.ident;
+    let builder_ident = format_ident!("{}Builder", input.ident);
+
+    let generated = quote! {
+
+        impl #ident {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {}
+            }
+        }
+
+        #vis struct #builder_ident {
+        }
+    };
+    generated.into()
 }
