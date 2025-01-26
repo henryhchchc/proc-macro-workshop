3698382ae26ba437f0e5361ffdd3446e22ae8786
Henry Chu <henrytech@outlook.com>
2025-01-26 22:10:38 +0800

    buiilder: test 03

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -10,15 +10,50 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let ident = &input.ident;
     let builder_ident = format_ident!("{}Builder", input.ident);
 
+    let Data::Struct(strukt) = input.data else {
+        panic!("Builder derive only supports struct");
+    };
+    let Fields::Named(fields) = strukt.fields else {
+        panic!("Builder derive only supports named fields");
+    };
+    let builder_fields: Vec<_> = fields.named.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+        let ty = &field.ty;
+        quote! { #ident: Option<#ty> }
+    }).collect();
+
+    let builder_init: Vec<_> = fields.named.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+        quote! { #ident: None }
+    }).collect();
+
+    let setter_methods: Vec<_> = fields.named.iter().map(|field| {
+        let ident = field.ident.as_ref().unwrap();
+        let ty = &field.ty;
+        quote! {
+            pub fn #ident(&mut self, #ident: #ty) -> &mut Self {
+                self.#ident = Some(#ident);
+                self
+            }
+        }
+    }).collect();
+
     let generated = quote! {
 
         impl #ident {
             pub fn builder() -> #builder_ident {
-                #builder_ident {}
+                #builder_ident {
+                    #(#builder_init),*
+                }
             }
         }
 
         #vis struct #builder_ident {
+            #(#builder_fields),*
+        }
+
+        impl #builder_ident {
+            #(#setter_methods)*
         }
     };
     generated.into()
